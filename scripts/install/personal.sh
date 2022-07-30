9211a0fc843c0b250f0c7672c714d15b1d03e0bf
Hashem A. Damrah <dermdorm2@gmail.com>
2022-07-30 10:25:58 -0700

    add personal.sh

---
+++ scripts/install/personal.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+source "$HOME/Singularis/bin/common"
+source "$HOME/Singularis/bin/variables"
+
+git clone https://github.com/SingularisArt/.minecraft ~/.minecraft
+git clone https://github.com/SingularisArt/.tlauncher ~/.tlauncher
+git clone https://github.com/SingularisArt/.gnupg ~/.gnupg
+git clone https://github.com/SingularisArt/.password-store ~/.password-store
+git clone https://github.com/SingularisArt/.ssh ~/.ssh
+
+git clone https://github.com/SingularisArt/journal ~/Documents/journal
+git clone https://github.com/SingularisArt/discipline-notes ~/Documents/discipline-notes
+git clone https://github.com/SingularisArt/school-notes ~/Documents/school-notes
+git clone https://github.com/SingularisArt/school-setup ~/Documents/school-setup
+git clone https://github.com/SingularisArt/Website ~/Documents/Website
+
+git clone https://github.com/SingularisArt/Desktop ~/Desktop
