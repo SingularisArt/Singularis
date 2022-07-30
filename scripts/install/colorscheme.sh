19e36d0950fd08f100dc84d6bb3840ef1da3a7b9
Hashem A. Damrah <dermdorm2@gmail.com>
2022-07-30 10:28:19 -0700

    add more things to colorscheme.sh

--- scripts/install/colorscheme.sh
+++ scripts/install/colorscheme.sh
@@ -5,8 +5,18 @@ source "$HOME/Singularis/bin/variables"
 
 log_notice "Generating colorscheme"
 
+log_info "Setting up start-tree"
+
 cd ${THIRD_PARTY_TOOLS}/start-tree
 ./init.sh
 ./generate.py
+
+log_info "Setting up GnuPlot"
+
+cd ${THIRD_PARTY_TOOLS}/gnuplot-pywal
+sudo bash install.sh
+
+log_info "Setting up everything else"
+
 cd ${THIRD_PARTY_TOOLS}/chameleon
 ./chameleon.py -i /home/singularis/Singularis/media/wallpapers/0942.jpg
