688a1b2d917170865533f2bbb26116790b9fddbb
Hashem A. Damrah <dermdorm2@gmail.com>
2022-08-05 18:04:22 -0700

    add bin/touchpad to enable touchpad on arch

--- scripts/install/settings.sh
+++ scripts/install/settings.sh
@@ -8,3 +8,8 @@ log_notice "Executing settings"
 log_info "Making zsh default shell"
 
 chsh -s $(which zsh)
+
+log_info "Enabling touchpad"
+
+mkdir /etc/X11/xorg.conf.d/
+sudo cp -r $BIN/touchpad /etc/X11/xorg.conf.d/70-synaptics.conf
