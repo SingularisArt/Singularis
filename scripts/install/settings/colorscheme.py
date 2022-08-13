c2be2fa44f3af1f3246e7329992fec96e14f859f
Hashem A. Damrah <dermdorm2@gmail.com>
2022-08-12 22:33:54 -0700

    restuctured everything to work with the new work in progress python scripts

---
+++ scripts/install/settings/colorscheme.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3.10
+
+# cd ${THIRD_PARTY_TOOLS}/start-tree
+# ./init.sh
+# ./generate.py
+
+# cd ${THIRD_PARTY_TOOLS}/gnuplot-pywal
+# sudo bash install.sh
+
+# cd ${THIRD_PARTY_TOOLS}/chameleon
+
+# wal-discord; beautifuldiscord --css ~/.cache/wal-discord/style.css
+
+# ./chameleon.py -i /home/singularis/Singularis/media/wallpapers/0942.jpg
+
+def main():
+    print('Colorscheme')
