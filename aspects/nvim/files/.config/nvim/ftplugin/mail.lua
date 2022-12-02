d77a533a0ba804aa5c75d623df26d3ee2e77d3de
Hashem A. Damrah <singularisartt@gmail.com>
2022-12-01 17:46:17 -0800

    edit email setup and add cool email send thing to neovim

---
+++ aspects/nvim/files/.config/nvim/ftplugin/mail.lua
@@ -0,0 +1,50 @@
+vim.opt.wrap = true
+vim.opt.tw = 0
+vim.opt.spell = true
+
+vim.cmd([[
+set omnifunc=mailcomplete#Complete
+
+function! s:new_mail()
+  let l:path = system('mktemp --tmpdir XXXXXXXXXX.eml')
+  execute 'e' l:path
+
+  startinsert
+
+  0r $HOME/.config/nvim/skeletons/mail.eml | w | $
+endfunction
+
+command! Mail call s:new_mail()
+
+function! s:OnExit(job_id, code, event) dict
+  if a:code == 0
+    execute 'bd!' s:bufnr
+    if len(getbufinfo({'buflisted': 1})) == 1
+      quit
+    endif
+  else
+    execute 'Bd!' s:bufnr
+  endif
+endfunction
+
+function! s:SendMail()
+  let l:message_file = expand('%')
+  if l:message_file != ''
+    update
+  else
+    let l:message_file = system('mktemp')
+    execute 'w!' l:message_file
+  endif
+  call termopen('neomutt ' .
+    \ "-e 'set postpone=no sidebar_visible=no assumed_charset=utf-8' " .
+    \ "-H " . l:message_file)
+endfunction
+
+nnoremap <buffer> <silent> <C-H> :<C-U>call <SID>SendMail()<CR>
+
+nnoremap <buffer> <silent> <localleader>f gg/From:<CR>:nohlsearch<CR>4lC: 
+nnoremap <buffer> <silent> <localleader>t gg/To:<CR>:nohlsearch<CR>2lC: 
+nnoremap <buffer> <silent> <localleader>c gg/Cc:<CR>:nohlsearch<CR>2lC: 
+nnoremap <buffer> <silent> <localleader>b gg/Bcc:<CR>:nohlsearch<CR>3lC: 
+nnoremap <buffer> <silent> <localleader>s gg/Subject:<CR>:nohlsearch<CR>7lC: 
+]])
